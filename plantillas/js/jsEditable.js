f8f2a57597059b4a13e0b66c32f4fd4101493549
daniela <rod.gondaniela0511@gmail.com>
2020-11-12 16:45:46 -0400

    editor

---
+++ plantillas/js/jsEditable.js
@@ -0,0 +1,29 @@
+
+function camb() {
+   document.getElementById('menus').style.border='dashed'
+   document.getElementById('header').style.border='none'
+   document.getElementById('produc').style.border='none'
+   document.getElementById('demasProd').style.border='none'
+
+ 
+}
+
+function header() {
+    document.getElementById('header').style.border='dashed'
+    document.getElementById('menus').style.border='none'
+    document.getElementById('produc').style.border='none'
+    document.getElementById('demasProd').style.border='none'
+ }
+ function productos() {
+    document.getElementById('produc').style.border='dashed'
+    document.getElementById('header').style.border='none'
+    document.getElementById('menus').style.border='none'
+    document.getElementById('demasProd').style.border='none'
+ }
+ function demasProd() {
+    document.getElementById('demasProd').style.border='dashed'
+    document.getElementById('produc').style.border='none'
+    document.getElementById('header').style.border='none'
+    document.getElementById('menus').style.border='none'
+ }
+
